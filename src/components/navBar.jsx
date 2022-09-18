0002fbbb4337f887320520c760ac7aae00b7fb63
Naveen <mnreddy.nv@gmail.com>
2022-09-18 07:00:03 +0530

    span changes

--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -6,7 +6,7 @@ const NavBar = ({totalCounters}) =>{
         <nav className="navbar navbar-light bg-light">
           <a className="navbar-brand" href="#">
             Navbar
-            <span className='badge badge-pill badge-secondary'>{totalCounters}</span>
+            <span className='badge badge-pill badge-secondary m-2'>{totalCounters}</span>
           </a>
         </nav>
       );
